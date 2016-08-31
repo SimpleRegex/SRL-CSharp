a235c1bce2a1f88f3e9ad4439aa25a641a4a53f3
Dennis Hernández <dennishernandezvargas@gmail.com>
2016-08-31 11:16:09 -0600

    Adding builders and .sln file

---
+++ src/SRL-CSharp/Builders/NonCapture.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SRL_CSharp.Builders
+{
+    public class NonCapture : Builder
+    {
+        /** @var string Desired non capture group. */
+        protected string group = "(?:{0})";
+    }
+}
---
+++ src/SRL-CSharp/Builders/PositiveLookahead.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SRL_CSharp.Builders
+{
+    public class PositiveLookahead : Builder
+    {
+        /** @var string Desired lookahead group. */
+        protected string group = "(?={0})";
+    }
+}
