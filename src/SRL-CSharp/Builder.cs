9e7cad0c4c54d22c30feb212e57031751309e031
Andreas Korb <Andreas.D.Korb@gmail.com>
2016-09-01 11:05:01 +0200

    Using correct type for methods in Builder.cs now

--- src/SRL-CSharp/Builder.cs
+++ src/SRL-CSharp/Builder.cs
@@ -1,30 +1,28 @@
 ﻿using SRL.Interfaces;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SRL
 {
     public class Builder : TestMethodProvider
     {
         protected const string NON_LITERAL_CHARACTERS = "[\\^$.|?*+()";
-        protected const string METHOD_TYPE_BEGIN = "0b00001";
-        protected const string METHOD_TYPE_CHARACTER = "0b00010";
-        protected const string METHOD_TYPE_GROUP = "0b00100";
-        protected const string METHOD_TYPE_QUANTIFIER = "0b01000";
-        protected const string METHOD_TYPE_ANCHOR = "0b10000";
-        protected const string METHOD_TYPE_UNKNOWN = "0b11111";
+
+        // Binary literals will come with C# 7.0
+        protected const int METHOD_TYPE_BEGIN = 1;      //"0b00001";
+        protected const int METHOD_TYPE_CHARACTER = 2;  //"0b00010";
+        protected const int METHOD_TYPE_GROUP = 4;      //"0b00100";
+        protected const int METHOD_TYPE_QUANTIFIER = 8; //"0b01000";
+        protected const int METHOD_TYPE_ANCHOR = 16;    //"0b10000";
+        protected const int METHOD_TYPE_UNKNOWN = 31;   //"0b11111";
         //protected const string METHOD_TYPES_ALLOWED_FOR_CHARACTERS = self::METHOD_TYPE_BEGIN | self::METHOD_TYPE_ANCHOR | self::METHOD_TYPE_GROUP | self::METHOD_TYPE_QUANTIFIER | self::METHOD_TYPE_CHARACTER;
 
         /** @var string[] RegEx being built. */
         protected List<string> regEx = new List<string>();
         /** @var string Raw modifiers to apply on get(). */
-        protected string modifiers = string.Empty;
+        protected string modifiers = "";
 
         /** @var int Type of last method, to avoid invalid builds. */
-        protected string lastMethodType = METHOD_TYPE_BEGIN;
+        protected int lastMethodType = METHOD_TYPE_BEGIN;
 
         protected Dictionary<string, string> modifierMapper = new Dictionary<string, string>
         {
